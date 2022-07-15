4cc564fc8b37561fe4bc43bac39ab89113e7b837
SeijoHuang <erinhuang817@gmail.com>
2022-07-16 01:00:00 +0800

    chore: import bootstrap grid system

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <template>
   <div id="app">
+    <Nav />
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
+<script>
+import 'bootstrap/dist/css/bootstrap-grid.min.css'
+import Nav from "./components/NavTab.vue"
+export default {
+  components: {
+    Nav
+  }  
+}
+</script>
 
+<style lang="scss">
+html {
+  background: black;
+}
+*{
+  color: $font-white;
+}
 </style>
