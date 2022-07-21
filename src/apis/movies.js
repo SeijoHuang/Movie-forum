ae2e78630cd1eb5484ccd2be33afacad07a218a2
SeijoHuang <erinhuang817@gmail.com>
2022-07-21 23:04:06 +0800

    feat: home billboards slides 串接movieApi取得billboards需求的電影資料

--- src/apis/movies.js
+++ src/apis/movies.js
@@ -1,13 +1,25 @@
 import { apiHelper } from "../utils/helpers";
 
 export default {
-  getLatest(){
+  getLatest() {
     return apiHelper.get("/movie/latest")
   },
-  getGenres(){
+  getGenres() {
     return apiHelper.get("/genre/movie/list")
   },
-  getTrending(){
-    return apiHelper.get("trending/movie/day")
+  getTrending() {
+    return apiHelper.get("/trending/movie/day")
+  },
+  getPopular() {
+    return apiHelper.get("/movie/popular")
+  },
+  getTopRated() {
+    return apiHelper.get("/movie/top_rated")
+  },
+  getNowPlaying(){
+    return apiHelper.get("/movie/now_playing")
+  },
+  getUpComing(){
+    return apiHelper.get("/movie/upcoming")
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,6 +10,7 @@
 
 <script>
 import MainSlide from "../components/MainSlide.vue"
+import moviesApi from "../apis/movies"
 export default {
   name: 'Home',
   components: {
@@ -18,7 +19,62 @@ export default {
   data(){
     return {
       isOpen: true,
+      billboards: [
+        {
+          title:"Popular",
+        },
+        {
+          title:"Top Rated"
+        },
+        {
+          title:"Now Playing"
+        },
+        {
+          title:"Up Coming"
+        },
+      ]
     }
+  },
+  methods: {
+    async getBillboards(){
+      try{
+        const {data: popular} = await moviesApi.getPopular()
+        const {data: topRated} = await moviesApi.getTopRated()
+        const {data: nowPlaying} = await moviesApi.getNowPlaying()
+        const {data: upComing} = await moviesApi.getUpComing()
+        const newBillboards = this.billboards.map( board => {
+          switch (board.title) {
+            case "Popular":
+              return {
+                ...board,
+                movies: [...popular.results]
+              }
+            case "Top Rated":
+              return {
+                ...board,
+                movies: [...topRated.results]
+              }
+            case "Now Playing":
+              return {
+                ...board,
+                movies: [...nowPlaying.results]
+              }
+            case "Up Coming":
+              return {
+                ...board,
+                movies: [...upComing.results]
+              }
+          }
+        })
+        this.billboards = [...newBillboards]
+      } catch(error) {
+        this.$toast('error', error.response.data.status_message)
+      }
+    }
+  },
+  mounted() {
+    this.getBillboards()
   }
 }
 </script>
+ 
