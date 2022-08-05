1a0d34a2c113e3b6bc220c0b916e0b241112df29
SeijoHuang <erinhuang817@gmail.com>
2022-08-05 15:20:15 +0800

    feat: 處理MovieModal和MovieCard顯示的資料格式 說明： store中mutation、action處理伺服器回傳的電影資料 1.無海報或backdrop時套用預設圖片 2.上映日期格式 3.評分人數換算 4.語言，少部分無顯示語言資料

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import movieApi from "../apis/movies"
 import { Toast } from "../utils/helpers"
+import { imgPath } from "../utils/helpers"
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -20,7 +21,12 @@ export default new Vuex.Store({
       vote_average: "",
       vote_count: "",
     },
-    searchResult:{}
+    searchResult:{
+      page:"",
+      results:[],
+      total_pages: "",
+      total_results:""
+    }
   },
   getters: {},
   mutations: {
@@ -35,10 +41,34 @@ export default new Vuex.Store({
       state.movieModalContent = data
     },
     setSearchResult(state, data) {
-      state.searchResult = data
+      const {page, results, total_pages, total_results} = data  
+      const newResults = results.map( result => {
+        const isVoterOver = result.vote_count / 1000 < 1
+        return {
+          ...result,
+          poster_path: result.poster_path
+            ? imgPath.poster + result.poster_path
+            : require("../assets/static/images/noPoster.png"),
+          backdrop_path: result.backdrop_path
+            ? imgPath.backdrop + result.backdrop_path
+            : require("../assets/static/images/noBackdrop.jpeg"),
+          release_date: result.release_date.slice(0, 4),
+          vote_count: isVoterOver
+            ? result.vote_count
+            : Math.round((result.vote_count / 1000) * 100) / 100 + "k ",
+        }
+      })
+      state.searchResult = {
+        ...state.searchResult,
+        page,
+        results: newResults,
+        total_pages,
+        total_results
+      }
     }
   },
   actions: {
+    //MovieModal資料
     async getMovieData({ commit }, movieId) {
       try {
         commit("changeLoadingState")
@@ -57,16 +87,18 @@ export default new Vuex.Store({
         } = data
 
         //判斷評分人數是否大於1000人
-        let isVoterOver = vote_count / 1000 < 1
+        const isVoterOver = vote_count / 1000 < 1
 
         commit("getMovieModalContent", {
           genres,
           id,
           overview,
-          poster_path,
+          poster_path: poster_path
+            ? imgPath.poster + poster_path
+            : require("../assets/static/images/noPoster.png"),
           release_date: release_date.slice(0, 4),
           runtime,
-          spoken_languages: spoken_languages[0].english_name,
+          spoken_languages: spoken_languages.length? spoken_languages[0].english_name : "no information",
           title,
           vote_average,
           vote_count: isVoterOver
@@ -75,20 +107,23 @@ export default new Vuex.Store({
         })
         commit("changeLoadingState")
       } catch (error) {
+        console.error.response
          Toast.fire({
            icon: "error",
            title: error.response.data.status_message,
          })
       }
     },
+    //取得搜尋電影資料
     async getSearchResult({commit}, {page, query}) {
       try{
+        commit("changeLoadingState")
         const { data } = await movieApi.search( {
           page,
           query
         })
-        console.log( data )
         commit("setSearchResult", data)
+        commit("changeLoadingState")
       } catch (error) {
         Toast.fire({
           icon: "error",
